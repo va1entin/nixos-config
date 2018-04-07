bff9d7f279691f974c348691796e6723f7005a14
Valentin Heidelberger <github@valentinsblog.com>
2018-04-07 20:55:28 +0200

    move system configs to separate file

--- configuration.nix
+++ configuration.nix
@@ -13,18 +13,10 @@
       /home/val/git/nixos-config/networking.nix
       /home/val/git/nixos-config/nix.nix
       /home/val/git/nixos-config/packages.nix
+      /home/val/git/nixos-config/system.nix
       /home/val/git/nixos-config/users.nix
       /home/val/git/nixos-config/xserver.nix
       /home/val/git/nixos-config/zsh.nix
     ];
-
-  system.autoUpgrade.enable = true;
-  system.autoUpgrade.dates = "19:00";
-
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.stateVersion = "17.09"; # Did you read the comment?
 
 }
---
+++ system.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+{
+system = {
+
+  autoUpgrade = {
+    enable = true;
+    dates = "19:00";
+  };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  stateVersion = "17.09"; # Did you read the comment?
+};
+}
