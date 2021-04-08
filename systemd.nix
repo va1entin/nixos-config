4fae8812a3677711bcdc12ef4b1f1c8f65832527
va1entin <valh@mailbox.org>
2021-04-08 15:42:51 +0200

    fix indentation

--- systemd.nix
+++ systemd.nix
@@ -1,12 +1,13 @@
 { config, pkgs, ... }:
 {
   systemd = {
-  extraConfig = ''
-    DefaultTimeoutStopSec=10s
-  '';
-  #  services.nvidia-control-devices = {
-  #    wantedBy = [ "multi-user.target" ];
-  #    serviceConfig.ExecStart = "${pkgs.linuxPackages.nvidia_x11.bin}/bin/nvidia-smi";
-  #  };
-  };
+    extraConfig = ''
+      DefaultTimeoutStopSec=10s
+    '';
+
+    #  services.nvidia-control-devices = {
+    #    wantedBy = [ "multi-user.target" ];
+    #    serviceConfig.ExecStart = "${pkgs.linuxPackages.nvidia_x11.bin}/bin/nvidia-smi";
+    #  };
+    };
 }
