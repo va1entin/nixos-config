70dc921584e4ac0d25271868379880207233222e
va1entin <valh@mailbox.org>
2021-04-08 15:42:00 +0200

    enabled bluetooth

--- hardware.nix
+++ hardware.nix
@@ -1,10 +1,29 @@
+{ pkgs, ... }:
 {
   hardware = {
-    pulseaudio.enable = true;
+    pulseaudio = {
+      enable = true;
+      package = pkgs.pulseaudioFull;
+
+      # Enabling extra codecs
+      extraModules = [ pkgs.pulseaudio-modules-bt ];
+    };
+
+    # See https://nixos.wiki/wiki/Bluetooth
+    bluetooth = {
+      enable = true;
+
+      # Enable A2DP Sink
+      config = {
+        General = {
+          Enable = "Source,Sink,Media,Socket";
+      };
+    };
 
     # Required for Wayland with GDM when using nvidia proprietary drivers, see xserver.nix
     # nvidia.modesetting.enable = true;
 
     # opengl.driSupport32Bit = true;
+    };
   };
 }
